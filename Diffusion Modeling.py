2de8138c5bd9f140c358d938eecd03d45bba6443
whtowbin <whtowbin@gmail.com>
2018-12-04 23:29:57 -0500

    Updated code before larger rewrite

--- Diffusion Modeling.py
+++ Diffusion Modeling.py
@@ -10,15 +10,16 @@ T_C = 1200;
 T_K = T_C+273;
 DH2O = 1e12*(10**(-5.4))*np.exp(-130000/(8.314*T_K))  # Ferriss diffusivity in um2/s
 # These are critical for stability of model. Must think about time steps and model points.
-N_points = 10
+N_points = 100
 profile_length = 1500 # Microns
 dX = profile_length / (N_points -1) # Microns
 
+Distances = [0+dX*Y - profile_length/2 for Y in range(N_points)]
 
 
 max_time_step = DH2O**2 / (4*dX)
 max_time_step
-dt = 1 #0.0973 # time step seconds
+dt = 0.5 #1 #0.0973 # time step seconds
 
 boundary = 0 #ppm
 initial_C = 10 # ppm
@@ -32,19 +33,22 @@ v_initial = v
 # In the future I can update this every timestep if temperature changes with time.
 # also update boundary conditions with time
 
-delta = (DH2O * dt)/ (dX **2)
-delta
-mat_base = np.zeros(N_points)
-mat_base[0] = 1 - 2*delta
-mat_base[1] = delta
+def diffusion_matrix(DH2O, dt, dx):
+    delta = (DH2O * dt)/ (dx**2)
 
-B = np.mat(la.toeplitz((mat_base)))
-B[0,0] , B[-1,-1]  = 1, 1
-B[0,1] , B[-1,-2] = 0 , 0
+    mat_base = np.zeros(N_points)
+    mat_base[0] = 1 - 2*delta
+    mat_base[1] = delta
+
+    B = np.mat(la.toeplitz((mat_base)))
+    B[0,0] , B[-1,-1]  = 1, 1
+    B[0,1] , B[-1,-2] = 0 , 0
+    return B
 
 # %%
-def time_steper(v_in, timesteps, boundaries):
-"""
+
+def time_steper(v_in, Diff_Matrix, timesteps, boundaries = None):
+    """
 Steps a finite element 1D diffusion model forward.
 
 parameters
@@ -56,29 +60,40 @@ boundaries : Boundary conditions
 Return
 --------------
  An updated concentration profile.
-"""
+ """
 
     v_loop = v_in
-    for x in range(timesteps):
-        v_loop = B * v_loop
+    for idx, x in enumerate(range(timesteps)):
+        v_loop = Diff_Matrix * v_loop
+        if boundaries is not None:
+            # this currently wont work. I need to make it so the boundaries and timesteps are the same.
+            v_loop[0], v_loop[-1] = boundaries[idx]
     return v_loop
 
- # Estimate how long olivine took to reach present concnetration. Use boundary condition at depthself.
- # put distance on points. Make center zero and then measure distance too rims
 
- # And calcuate boundary condition for different partitioning values.
- # Cacluate length of Stalling ... 0.5 - 2 hours...
- # Caculate time for ascent ... ascent rate and change in pressure. from storage depth to surface. + Surface cooling ~ 10 minutes.
+B = diffusion_matrix(DH2O,dt,dX)
 
- # give min time range for olivine water loss since ascent started.
+fig, ax = plt.subplots(figsize=(12,8))
+plt.plot(Distances, time_steper(v, B, 0) )
+plt.plot(Distances, time_steper(v, B, 10))
+plt.plot(Distances, time_steper(v, B, 100))
+plt.plot(Distances, time_steper(v, B, 1000))
+plt.plot(Distances, time_steper(v, B, 10000))
 
- # Cacluate time for CPX to lose water
- # Measure Profile for Opx too.
+ax.set_xlabel("Distance to center of crystal ")
+ax.set_ylabel("ppm water")
 
-# Conclude that if CPX and Opx can lose water in this time scale they very likely equilibrated with the magma during ascent.
+#plt.savefig(" ")
 
-plt.plot(time_steper(v,0))
-plt.plot(time_steper(v,10))
-plt.plot(time_steper(v,100))
-plt.plot(time_steper(v,1000))
-plt.plot(time_steper(v,5000))
+D_OPX = 1e12*(10**(-3))*np.exp(-181000/(8.314*T_K))  * 10
+D_CPX = 1e12*(10**(-3))*np.exp(-181000/(8.314*T_K))  * 100
+
+
+
+
+
+# %%
+# Max water in ol at depth
+Intial_low = 2.7 * 0.0007 * 10000
+Initial_high = 2.7 * 0.0015 * 10000
+Final = 10
---
+++ ToDo_diff_code.md
@@ -0,0 +1,19 @@
+# Diffusion code to do list for meeting with Terry.
+- Estimate how long olivine took to reach present concnetration. Use boundary condition at depthself.
+- Put distance on points. Make center zero and then measure distance too rims
+- calcuate boundary condition for different partitioning values.
+- Cacluate length of Stalling ... 0.5 - 2 hours...
+- Caculate time for ascent ... ascent rate and change in pressure. from storage depth to surface. + Surface cooling ~ 10 minutes.
+
+- give min time range for olivine water loss since ascent started.
+
+- Cacluate time for CPX to lose water
+- Measure Profile for Opx too.
+- Conclude that if CPX and Opx can lose water in this time scale they very likely equilibrated with the magma during ascent.
+
+
+- I am unsure what Diffusivity to use for CPX. Elizabeth has several. Wood et al . 2000? somewhere between 10^-11.5 and 10^-12
+
+- What Should I assume for the Opx diffusivity? 10^-11 ?
+
+For now I will use my code for the beginning then megan's code to finish the fits. 
